625299e820241d54b225319638729871a837b1d1
Brandon Wischmeier <brandonwischmeier@gmail.com>
2020-04-14 18:31:53 -0400

    Updated activities

--- app/src/main/java/edu/cs/uga/roommatesshopping/MainActivity.java
+++ app/src/main/java/edu/cs/uga/roommatesshopping/MainActivity.java
@@ -1,14 +1,69 @@
 package edu.cs.uga.roommatesshopping;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button signOutBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        signOutBtn = findViewById(R.id.signOutBtn);
+        signOutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signOut();
+            }
+        });
+    }
+
+    /**
+     * Sign user out of Firebase Authentication and all social identity providers
+     */
+    public void signOut() {
+        AuthUI.getInstance()
+                .signOut(this)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                       signOutIntent();
+                    }
+                });
+    }
+
+    /**
+     * Helper method for signOut
+     * All this function does is start the login activity
+     */
+    private void signOutIntent() {
+        startActivity(new Intent(this, LoginActivity.class));
+        finish();
+    }
+
+    /**
+     * Completely delete user's account
+     */
+    public void deleteAccount() {
+        AuthUI.getInstance()
+                .delete(this)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        // TODO
+                    }
+                });
     }
 }
