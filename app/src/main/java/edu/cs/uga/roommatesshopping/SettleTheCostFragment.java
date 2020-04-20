285c123fd563c8f66091be44e1cde08057f6623b
Brandon Wischmeier <bdw26588@uga.edu>
2020-04-20 14:56:34 -0400

    Update

---
+++ app/src/main/java/edu/cs/uga/roommatesshopping/SettleTheCostFragment.java
@@ -0,0 +1,25 @@
+package edu.cs.uga.roommatesshopping;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class SettleTheCostFragment extends Fragment {
+
+    public SettleTheCostFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_settle_the_cost, container, false);
+    }
+}
