1a3026096e381759eb32825b0ffe22aabe0b0792
Brandon Wischmeier <bdw26588@uga.edu>
2020-04-20 12:43:08 -0400

    Update

--- app/src/main/java/edu/cs/uga/roommatesshopping/ChatFragment.java
+++ app/src/main/java/edu/cs/uga/roommatesshopping/ChatFragment.java
@@ -8,8 +8,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 
 public class ChatFragment extends Fragment {
+
+    private FirebaseDatabase database;
+    private DatabaseReference databaseReference;
 
     public ChatFragment() {
         // Required empty public constructor
--- app/src/main/java/edu/cs/uga/roommatesshopping/HomeFragment.java
+++ app/src/main/java/edu/cs/uga/roommatesshopping/HomeFragment.java
@@ -2,16 +2,24 @@ package edu.cs.uga.roommatesshopping;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.navigation.NavController;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+import java.util.ArrayList;
+
+import edu.cs.uga.roommatesshopping.databinding.FragmentHomeBinding;
 
 
 public class HomeFragment extends Fragment {
 
-    NavController controller;
+    private FragmentHomeBinding homeBinding;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -19,10 +27,42 @@ public class HomeFragment extends Fragment {
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        homeBinding = FragmentHomeBinding.inflate(inflater, container, false);
+        homeBinding.recyclerviewShoppingLists.setLayoutManager(new LinearLayoutManager(getContext()));
+        ShoppingListAdapter adapter = new ShoppingListAdapter(generateFakeValues());
+        homeBinding.recyclerviewShoppingLists.setAdapter(adapter);
+        return homeBinding.getRoot();
     }
 
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.fragment_home, menu);
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    private void getRecyclerView() {
+
+    }
+
+    private ArrayList<String> generateFakeValues() {
+        ArrayList<String> values = new ArrayList<>();
+        values.add("Shopping list 1");
+        values.add("Shopping list 2");
+        values.add("Shopping list 3");
+        values.add("Shopping list 4");
+        values.add("Shopping list 5");
+        values.add("Shopping list 6");
+        values.add("Shopping list 7");
+        values.add("Shopping list 8");
+        values.add("Shopping list 9");
+        return values;
+    }
 }
