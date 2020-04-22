24b557f0e013242afd2e296bcd2d249046f32720
megschmidt <megan.elizabeth.schmidt@aol.com>
2020-04-22 17:39:07 -0400

    began work on settleTheCost

--- app/src/main/java/edu/cs/uga/roommatesshopping/fragment/SettleTheCostFragment.java
+++ app/src/main/java/edu/cs/uga/roommatesshopping/fragment/SettleTheCostFragment.java
@@ -3,16 +3,33 @@ package edu.cs.uga.roommatesshopping.fragment;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
 import edu.cs.uga.roommatesshopping.R;
+import edu.cs.uga.roommatesshopping.pojo.ShoppingItem;
 
 
 public class SettleTheCostFragment extends Fragment {
-
+    ArrayList shoppingItemList;
+    RecyclerView recyclerView;
+    RecyclerView.LayoutManager layoutManager;
+    public static final String DEBUG_TAG = "SettleTheCostFragment";
     public SettleTheCostFragment() {
         // Required empty public constructor
     }
@@ -22,6 +39,43 @@ public class SettleTheCostFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settle_the_cost, container, false);
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+
+
+        // use a linear layout manager for the recycler view
+
+        DatabaseReference myRef = database.getReference("shoppingItems");
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        shoppingItemList = new ArrayList<ShoppingItem>();
+        View v = inflater.inflate(R.layout.fragment_settle_the_cost, container, false);
+        recyclerView = (RecyclerView) v.findViewById( R.id.recyclerView );
+        layoutManager = new LinearLayoutManager(getActivity().getApplicationContext());
+        recyclerView.setLayoutManager( layoutManager );
+        myRef.addListenerForSingleValueEvent( new ValueEventListener() {
+
+
+            @Override
+            public void onDataChange( DataSnapshot snapshot ) {
+                // Once we have a DataSnapshot object, knowing that this is a list,
+                // we need to iterate over the elements and place them on a List.
+                for( DataSnapshot postSnapshot: snapshot.getChildren() ) {
+                    ShoppingItem si = postSnapshot.getValue(ShoppingItem.class);
+                   shoppingItemList.add(si);
+                    Log.d( DEBUG_TAG, "ReviewJobLeadsActivity.onCreate(): added: " + si );
+                }
+                Log.d( DEBUG_TAG, "ReviewJobLeadsActivity.onCreate(): setting recyclerAdapter" );
+
+                // Now, create a JobLeadRecyclerAdapter to populate a ReceyclerView to display the job leads.
+                //recyclerAdapter = new JobLeadRecyclerAdapter( jobLeadsList );
+                //recyclerView.setAdapter( recyclerAdapter );
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                System.out.println("The read failed: " + databaseError.getMessage());
+            }
+        } );
+        return v;
     }
 }
