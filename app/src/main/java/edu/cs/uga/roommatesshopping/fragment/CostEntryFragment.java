1f45c42cb05f080ca59c29743d027c5bd0d08ec0
Brandon Wischmeier <bdw26588@uga.edu>
2020-04-25 16:18:22 -0400

    Update

--- app/src/main/java/edu/cs/uga/roommatesshopping/fragment/CostEntryFragment.java
+++ app/src/main/java/edu/cs/uga/roommatesshopping/fragment/CostEntryFragment.java
@@ -1,7 +1,6 @@
 package edu.cs.uga.roommatesshopping.fragment;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,8 +10,6 @@ import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -20,46 +17,31 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+
 import edu.cs.uga.roommatesshopping.R;
 import edu.cs.uga.roommatesshopping.pojo.ShoppingItem;
 import edu.cs.uga.roommatesshopping.pojo.UserPair;
 
 public class CostEntryFragment extends Fragment {
-<<<<<<< HEAD
-    EditText editText;
-    Button button;
-    ShoppingItem currentItem;
-=======
 
     private static final String TAG = "CostEntryFragment";
     private EditText editText;
     private Button button;
     private ShoppingItem currentItem;
-    //private HomeFragment homeFragment = new HomeFragment();
+    private ArrayList<ShoppingItem> shoppingItemList;
 
->>>>>>> 68e4e33fc53de8959a41fbaf59e3a3ad080cef4b
     public CostEntryFragment() {
     }
 
-    //
-//    public static CostEntryFragment newInstance(int index) {
-//        CostEntryFragment f = new CostEntryFragment();
-//        // Supply index input as an argument.
-//        Bundle args = new Bundle();
-//        args.putInt("index", index);
-//        f.setArguments(args);
-//        return f;
-//    }
+
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_cost_entry, container, false);
-<<<<<<< HEAD
-        Bundle args = getArguments();
-        assert args != null;
-        int index = args.getInt("index", 0);
+
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference myRef = database.getReference("shoppingItems");
-        final ArrayList<ShoppingItem> shoppingItemList = new ArrayList<ShoppingItem>();
+        shoppingItemList = getArguments().getParcelableArrayList("list");
         myRef.addListenerForSingleValueEvent( new ValueEventListener() {
 
             @Override
@@ -77,15 +59,8 @@ public class CostEntryFragment extends Fragment {
                 System.out.println("The read failed: " + databaseError.getMessage());
             }
         } );
-        currentItem = (ShoppingItem) shoppingItemList.get(index);
-        editText = v.findViewById(R.id.cost);
-        button = v.findViewById(R.id.button2);
-        button.setOnClickListener(new CostEntryFragment.clickListener());
-=======
-//        Bundle args = getArguments();
-//        int index = args.getInt("index", 0);
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("shoppingItems");
+
+
         FirebaseAuth mAuth = FirebaseAuth.getInstance();
         currentItem = (ShoppingItem) getArguments().getParcelableArrayList("list").get(getArguments().getInt("position"));
         editText = v.findViewById(R.id.cost);
@@ -111,18 +86,18 @@ public class CostEntryFragment extends Fragment {
         currentItem.setPurchasedUser(new UserPair(mAuth.getCurrentUser()));
         return v;
     }
-    private class clickListener implements View.OnClickListener
-    {
-        @Override
-        public void onClick(View v) {
-            FirebaseDatabase database = FirebaseDatabase.getInstance();
-            DatabaseReference myRef = database.getReference("shoppingItems");
-            FirebaseAuth mAuth = FirebaseAuth.getInstance();
-            double price = Double.parseDouble(editText.getText().toString());
-            currentItem.setPrice(price);
-            currentItem.setPurchasedUser(new UserPair(mAuth.getCurrentUser()));
-            //TODO: update in database
-
-        }
-    }
+//    private class clickListener implements View.OnClickListener
+//    {
+//        @Override
+//        public void onClick(View v) {
+//            FirebaseDatabase database = FirebaseDatabase.getInstance();
+//            DatabaseReference myRef = database.getReference("shoppingItems");
+//            FirebaseAuth mAuth = FirebaseAuth.getInstance();
+//            double price = Double.parseDouble(editText.getText().toString());
+//            currentItem.setPrice(price);
+//            currentItem.setPurchasedUser(new UserPair(mAuth.getCurrentUser()));
+//            //TODO: update in database
+//
+//        }
+//    }
 }
