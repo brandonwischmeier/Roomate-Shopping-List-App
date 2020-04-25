051352426970de89db60821d892ea0d4fe9ffcb0
Brandon Wischmeier <bdw26588@uga.edu>
2020-04-25 15:06:05 -0400

    Fixed navigation issue to CostEntryFragment

--- app/src/main/java/edu/cs/uga/roommatesshopping/fragment/CostEntryFragment.java
+++ app/src/main/java/edu/cs/uga/roommatesshopping/fragment/CostEntryFragment.java
@@ -12,37 +12,39 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-import java.util.ArrayList;
-
 import edu.cs.uga.roommatesshopping.R;
 import edu.cs.uga.roommatesshopping.pojo.ShoppingItem;
-import edu.cs.uga.roommatesshopping.pojo.UserPair;
 
 public class CostEntryFragment extends Fragment {
+
+    private static final String TAG = "CostEntryFragment";
+    //private HomeFragment homeFragment = new HomeFragment();
+
     public CostEntryFragment() {
     }
-    public static CostEntryFragment newInstance(int index) {
-        CostEntryFragment f = new CostEntryFragment();
-        // Supply index input as an argument.
-        Bundle args = new Bundle();
-        args.putInt("index", index);
-        f.setArguments(args);
-        return f;
-    }
+
+//
+//    public static CostEntryFragment newInstance(int index) {
+//        CostEntryFragment f = new CostEntryFragment();
+//        // Supply index input as an argument.
+//        Bundle args = new Bundle();
+//        args.putInt("index", index);
+//        f.setArguments(args);
+//        return f;
+//    }
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_cost_entry, container, false);
-        Bundle args = getArguments();
-        int index = args.getInt("index", 0);
+//        Bundle args = getArguments();
+//        int index = args.getInt("index", 0);
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference myRef = database.getReference("shoppingItems");
         FirebaseAuth mAuth = FirebaseAuth.getInstance();
-        ArrayList<ShoppingItem> shoppingItemList = new ArrayList<ShoppingItem>();
-        ShoppingItem currentItem = (ShoppingItem) shoppingItemList.get(index);
+        ShoppingItem currentItem = (ShoppingItem) getArguments().getParcelableArrayList("list").get(getArguments().getInt("position"));
         EditText editText = v.findViewById(R.id.cost);
-        double price = Double.parseDouble(editText.getText().toString());
-        currentItem.setPrice(price);
-        currentItem.setPurchasedUser(new UserPair(mAuth.getCurrentUser()));
+//        double price = Double.parseDouble(editText.getText().toString());
+//        currentItem.setPrice(price);
+//        currentItem.setPurchasedUser(new UserPair(mAuth.getCurrentUser()));
         return v;
     }
 }
--- app/src/main/java/edu/cs/uga/roommatesshopping/pojo/ShoppingItem.java
+++ app/src/main/java/edu/cs/uga/roommatesshopping/pojo/ShoppingItem.java
@@ -1,9 +1,12 @@
 package edu.cs.uga.roommatesshopping.pojo;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import com.firebase.ui.auth.data.model.User;
 import com.google.firebase.auth.FirebaseUser;
 
-public class ShoppingItem {
+public class ShoppingItem implements Parcelable {
     private String name;
     private double price;
     private FirebaseUser enteredUser;
@@ -19,6 +22,26 @@ public class ShoppingItem {
         this.purchasedUser = purchasedUser;
         this.purchased = purchased;
     }
+
+    protected ShoppingItem(Parcel in) {
+        name = in.readString();
+        price = in.readDouble();
+        enteredUser = in.readParcelable(FirebaseUser.class.getClassLoader());
+        purchased = in.readByte() != 0;
+    }
+
+    public static final Creator<ShoppingItem> CREATOR = new Creator<ShoppingItem>() {
+        @Override
+        public ShoppingItem createFromParcel(Parcel in) {
+            return new ShoppingItem(in);
+        }
+
+        @Override
+        public ShoppingItem[] newArray(int size) {
+            return new ShoppingItem[size];
+        }
+    };
+
     public String getName()
     {
         return name;
@@ -38,4 +61,17 @@ public class ShoppingItem {
     public void setPrice(double price){this.price = price;}
     public void setPurchasedUser(UserPair userPair){this.purchasedUser=userPair;}
     public void setPurchased(){purchased = true;}
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(name);
+        dest.writeDouble(price);
+        dest.writeParcelable(enteredUser, flags);
+        dest.writeByte((byte) (purchased ? 1 : 0));
+    }
 }
