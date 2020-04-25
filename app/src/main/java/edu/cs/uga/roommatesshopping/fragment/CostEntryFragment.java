2ff88f998b685302e46bd001477558537629a789
Brandon Wischmeier <bdw26588@uga.edu>
2020-04-25 16:07:12 -0400

    Update cost entry fragment

--- app/src/main/java/edu/cs/uga/roommatesshopping/fragment/CostEntryFragment.java
+++ app/src/main/java/edu/cs/uga/roommatesshopping/fragment/CostEntryFragment.java
@@ -4,7 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 
@@ -14,16 +16,20 @@ import com.google.firebase.database.FirebaseDatabase;
 
 import edu.cs.uga.roommatesshopping.R;
 import edu.cs.uga.roommatesshopping.pojo.ShoppingItem;
+import edu.cs.uga.roommatesshopping.pojo.UserPair;
 
 public class CostEntryFragment extends Fragment {
 
     private static final String TAG = "CostEntryFragment";
+    private EditText editText;
+    private Button button;
+    private ShoppingItem currentItem;
     //private HomeFragment homeFragment = new HomeFragment();
 
     public CostEntryFragment() {
     }
 
-//
+    //
 //    public static CostEntryFragment newInstance(int index) {
 //        CostEntryFragment f = new CostEntryFragment();
 //        // Supply index input as an argument.
@@ -40,11 +46,28 @@ public class CostEntryFragment extends Fragment {
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference myRef = database.getReference("shoppingItems");
         FirebaseAuth mAuth = FirebaseAuth.getInstance();
-        ShoppingItem currentItem = (ShoppingItem) getArguments().getParcelableArrayList("list").get(getArguments().getInt("position"));
-        EditText editText = v.findViewById(R.id.cost);
-//        double price = Double.parseDouble(editText.getText().toString());
-//        currentItem.setPrice(price);
-//        currentItem.setPurchasedUser(new UserPair(mAuth.getCurrentUser()));
+        currentItem = (ShoppingItem) getArguments().getParcelableArrayList("list").get(getArguments().getInt("position"));
+        editText = v.findViewById(R.id.cost);
+        button = v.findViewById(R.id.enterBtn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // prevent user from entering in a non-value
+                if (!editText.getText().toString().trim().isEmpty()) {
+                    double price = Double.parseDouble(editText.getText().toString());
+                    currentItem.setPrice(price);
+                    Toast.makeText(getActivity(),
+                            "You've set the price to $" + price, Toast.LENGTH_SHORT).show();
+                } else {
+                    editText.setText("");
+                    Toast.makeText(getActivity(),
+                            "Please enter a price", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        currentItem.setPurchasedUser(new UserPair(mAuth.getCurrentUser()));
         return v;
     }
 }
